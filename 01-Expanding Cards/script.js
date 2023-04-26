c725f77daa0fc888f45683de62d61ef6a6504fa0
JessJiang <jessjiang890@gmail.com>
2023-04-26 17:11:47 +1000

    first two projects

---
+++ 01-Expanding Cards/script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll(".panel");
+
+// it will select to a NodeList
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
